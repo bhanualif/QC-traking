fd6124f8ac86ec38d967e90876d2c7c4f95a983c
IvanRaflideo <ardyatamafadel68@gmail.com>
2025-11-10 14:52:20 +0700

    update for prod

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 // === Path setup ===
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const frontendPath = path.join(__dirname, "../frontend");
+const frontendPath = path.join(__dirname, "./");
 app.use(express.static(frontendPath));
 
 // ====== DATA IN-MEMORY ======
